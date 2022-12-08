163726ed5349c42b4b0ab14ef4dc2fc7771d7c4d
Frans Spijkerman <frans@spijkerman.nl>
2022-12-08 14:51:11 +0100

    More security

--- index.php
+++ index.php
@@ -1,40 +1,51 @@
 <?php
+$title = 'Task Manager 1.2';
+
+// show errors in browser during development
 session_start();
-ini_set('display_errors', 1.01);
+ini_set('display_errors', -1);
+
+// force refresh in browser during development
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1
 header("Pragma: no-cache"); // HTTP 1.0
 header("Expires: 0"); // Proxies
+
 // set include path to work from any directory level
 set_include_path('./' . PATH_SEPARATOR . '../');
 
-$title = 'Task Manager 1.01';
-
+// requested controller should be in known controllers
 $controller = filter_input(INPUT_GET, 'controller');
-if (!empty($controller)) {
-    require "controller/{$controller}.php";
+$controllers = [
+    'TaskController',
+    'ProjectController',
+    'ResetController',
+];
+if (in_array($controller, $controllers)) {
+    require("controller/$controller.php");
     exit;
 }
 
+// requested view should be in allowed
 $view = filter_input(INPUT_GET, 'view');
-if (empty($view)) {
-    $view = 'Home';
-} else {
-    $cleanview = str_replace(['.', '/', '\\'], '', $view);
-    if ($cleanview !== $view) {
-        header("location: ?view=$cleanview");
-    }
-}
-
 $views = [
-    'Home' => 'Home',
-    'ProjectList' => 'Projects',
-    'TaskList' => 'Tasks',
+    // allowed views
+    'Home',
+    'ProjectList',
+    'TaskList',
+    'TaskEdit',
+    'ProjectEdit',
 ];
-
-// requested view should be in allowed views
-if (!array_key_exists($view, $views)) {
+if (!in_array($view, $views)) {
     header('location: ?view=Home');
 }
+
+// views available in menu
+$menu = [
+    // item => view
+    'Home' => 'Home',
+    'Projects' => 'ProjectList',
+    'Tasks' => 'TaskList',
+];
 ?>
 <!doctype html>
 <html lang='nl'>
@@ -54,9 +65,9 @@ if (!array_key_exists($view, $views)) {
         </h1>
     </header>
     <nav>
-        <?php foreach ($views as $name => $title) { ?>
-        <a href="?view=<?= $name ?>">
-            <?= $title ?>
+        <?php foreach ($menu as $menu_item => $menu_view) { ?>
+        <a href="?view=<?= $menu_view ?>">
+            <?= $menu_item ?>
         </a>
         <?php } ?>
         <a href="?controller=ResetController"
@@ -69,7 +80,7 @@ if (!array_key_exists($view, $views)) {
         ?>
     </main>
     <footer>
-        <p>&copy; Frans Spijkerman 2020 - Sources staan op <a target="github"
+        <p>&copy; Frans Spijkerman 2020-2022 - Sources staan op <a target="github"
                 href="https://github.com/spijkerbak/project-manager-1">Github</a></p>
     </footer>
 </body>
